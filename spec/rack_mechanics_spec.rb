c9f6d929694d6353564464cdd56a362f5e5460bd
chejianchao <820992376@qq.com>
2018-08-05 12:19:11 -0400

    Done.

--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper.rb'
 describe "Application" do
   it "should introduce itself" do
     get "/"
-    binding.pry
     expect(last_response.body).to include ("Hello, my name is")
   end
 end
